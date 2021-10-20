5d14d5bc3a8deffdda959092ed71448e6f1e9dac
Drew Henderson <drewhendersonemail@gmail.com>
2021-10-20 15:35:47 -0700

    added reading the buttons
    Co-authored-by: Karen Axon <kaxonwa@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,48 @@
+function add(number1, number2){
+  return number1 + number2;
+}
+
+function subtract(number1, number2){
+  return number1 - number2;
+}
+
+function mutiply(number1, number2){
+  return number1 * number2;
+}
+
+function divide(number1, number2){
+  return number1 / number2;
+}
+
+$(document).ready(function(){
+  $("mainForm").submit()
+    event.preventDefault();
+  
+  const number1 = parseInt("button#zero");
+  const number1 = parseInt("button#one");
+  const number1 = parseInt("button#two");
+  const number1 = parseInt("button#three");
+  const number1 = parseInt("button#four");
+  const number1 = parseInt("button#five");
+  const number1 = parseInt("button#six");
+  const number1 = parseInt("button#seven");
+  const number1 = parseInt("button#eight");
+  const number1 = parseInt("button#nine");
+  
+  const operation = parseInt("button#add");
+  const operation = parseInt("button#subract");
+  const operation = parseInt("button#multiply");
+  const operation = parseInt("button#divide");
+  
+  const number2 = parseInt("button#zero");
+  const number2 = parseInt("button#one");
+  const number2 = parseInt("button#two");
+  const number2 = parseInt("button#three");
+  const number2 = parseInt("button#four");
+  const number2 = parseInt("button#five");
+  const number2 = parseInt("button#six");
+  const number2 = parseInt("button#seven");
+  const number2 = parseInt("button#eight");
+  const number2 = parseInt("button#nine");
+  
+});
